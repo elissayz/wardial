6c8418bba81eb0a5ee110175ea45a06af6b2edb7
Elissa Yanez <Elissa.Yanez.25@cmc.edu>
2023-05-13 22:08:54 -0700

    wardial3

--- wardial.py
+++ wardial.py
@@ -214,7 +214,7 @@ async def _wardial_async(hosts, max_connections=500, timeout=10, schema='http'):
         results = []
         for host in hosts:
             results.append(is_server_at_host(session,host))
-        return await ayncio.gather(*results)
+        return await asyncio.gather(*results)
 
 
 def wardial(hosts, **kwargs):
